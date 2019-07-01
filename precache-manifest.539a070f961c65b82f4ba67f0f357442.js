9b0aeb1f4489fd7d80e941061071e2098a091aea
Mohammad Toosi <m_dd@rogers.com>
2019-07-01 12:09:09 +0430

    Updates

---
+++ precache-manifest.539a070f961c65b82f4ba67f0f357442.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c31962ac20ed1906f1973cac37ac968",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7279e2232eb5078db505",
+    "url": "/static/css/main.84465dcf.chunk.css"
+  },
+  {
+    "revision": "619648ba3537a94549df",
+    "url": "/static/js/2.f65516d1.chunk.js"
+  },
+  {
+    "revision": "7279e2232eb5078db505",
+    "url": "/static/js/main.59e5c618.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8f95ad31346e8dc64fcdf6604f2aa663",
+    "url": "/static/media/avatar.8f95ad31.jpg"
+  },
+  {
+    "revision": "4bced7c4c0d61d4f988629bb8ae80b8b",
+    "url": "/static/media/fa-solid-900.4bced7c4.woff"
+  },
+  {
+    "revision": "acf50f59802f20d8b45220eaae532a1c",
+    "url": "/static/media/fa-solid-900.acf50f59.ttf"
+  },
+  {
+    "revision": "b557f56e367e59344ca95f9d1fb44352",
+    "url": "/static/media/fa-solid-900.b557f56e.svg"
+  },
+  {
+    "revision": "b5cf8ae26748570d8fb95a47f46b69e1",
+    "url": "/static/media/fa-solid-900.b5cf8ae2.woff2"
+  },
+  {
+    "revision": "ef3df98419d143d9617fe163bf4edc0b",
+    "url": "/static/media/fa-solid-900.ef3df984.eot"
+  }
+]);
