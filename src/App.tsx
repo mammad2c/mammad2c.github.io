b562c2cf931fa67325d53d1860d00bb1b23b36a9
Mohammad Toosi <m_dd@rogers.com>
2019-10-24 12:22:17 +0330

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -5,43 +5,39 @@ import Timeline from "./components/Timeline/Timeline";
 import Skill from "./components/Skill/Skill";
 
 const App: React.FC = () => {
-    return (
-        <div className="container">
-            <div className="bio-box">
-                <div className="row">
-                    <div className="col-12 col-lg-8">
-                        <section className="my-info">
-                            <div className="row align-items-center">
-                                <div className="col-12 col-md-4">
-                                    <img
-                                        className="avatar img-fluid"
-                                        src={MyAvatar}
-                                        alt="Mohammad Toosi"
-                                    />
-                                </div>
-                                <div className="col-12 col-md-8">
-                                    <div className="ml-30">
-                                        <h1 className="my-name">
-                                            Mohammad Toosi
-                                        </h1>
-                                        <h3 className="my-expert">
-                                            Front-End Developer
-                                        </h3>
-                                    </div>
-                                </div>
-                            </div>
-                        </section>
-                        <div className="bio-box__timeline-wrapper">
-                            <Timeline />
-                        </div>
-                    </div>
-                    <div className="col-12 col-lg-4">
-                        <Skill />
-                    </div>
+  return (
+    <div className="container">
+      <div className="bio-box">
+        <div className="row">
+          <div className="col-12 col-lg-8">
+            <section className="my-info">
+              <div className="row align-items-center">
+                <div className="col-12 col-md-4">
+                  <img
+                    className="avatar img-fluid"
+                    src={MyAvatar}
+                    alt="Mohammad Toosi"
+                  />
                 </div>
+                <div className="col-12 col-md-8">
+                  <div className="ml-30">
+                    <h1 className="my-name">Mohammad Toosi</h1>
+                    <h3 className="my-expert">Front-End Developer</h3>
+                  </div>
+                </div>
+              </div>
+            </section>
+            <div className="bio-box__timeline-wrapper">
+              <Timeline />
             </div>
+          </div>
+          <div className="col-12 col-lg-4">
+            <Skill />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- src/components/Skill/Skill.tsx
+++ src/components/Skill/Skill.tsx
@@ -2,128 +2,128 @@ import React from "react";
 import SkillBox from "./SkillBox";
 
 const skills = [
-    {
-        title: "ReactJs",
-        rate: 4,
-    },
-    {
-        title: "React Native",
-        rate: 4,
-    },
-    {
-        title: "React Redux",
-        rate: 5,
-    },
-    {
-        title: "Redux Saga",
-        rate: 4,
-    },
-    {
-        title: "Redux Thunk",
-        rate: 3,
-    },
-    {
-        title: "Redux Persist",
-        rate: 2,
-    },
-    {
-        title: "Immutable Js",
-        rate: 4,
-    },
-    {
-        title: "VueJs",
-        rate: 3,
-    },
-    {
-        title: "Vuex",
-        rate: 2,
-    },
-    {
-        title: "Ruby on Rails",
-        rate: 2,
-    },
-    {
-        title: "Typescript",
-        rate: 4,
-    },
-    {
-        title: "Gulp Js",
-        rate: 5,
-    },
-    {
-        title: "Webpack",
-        rate: 3,
-    },
-    {
-        title: "Bootstrap",
-        rate: 5,
-    },
-    {
-        title: "SASS/LESS",
-        rate: 5,
-    },
-    {
-        title: "Node Js",
-        rate: 3,
-    },
-    {
-        title: "CSS/CSS3",
-        rate: 5,
-    },
-    {
-        title: "HTML/HTML5",
-        rate: 5,
-    },
-    {
-        title: "Git",
-        rate: 4,
-    },
-    {
-        title: "Express Js",
-        rate: 3,
-    },
-    {
-        title: "Adobe Photoshop",
-        rate: 3,
-    },
-    {
-        title: "Adobe Illustrator",
-        rate: 2,
-    },
-    {
-        title: "Adobe XD",
-        rate: 3,
-    },
+  {
+    title: "ReactJs",
+    rate: 4
+  },
+  {
+    title: "React Native",
+    rate: 4
+  },
+  {
+    title: "React Redux",
+    rate: 5
+  },
+  {
+    title: "Redux Saga",
+    rate: 4
+  },
+  {
+    title: "Redux Thunk",
+    rate: 3
+  },
+  {
+    title: "Redux Persist",
+    rate: 2
+  },
+  {
+    title: "Immutable Js",
+    rate: 4
+  },
+  {
+    title: "VueJs",
+    rate: 3
+  },
+  {
+    title: "Vuex",
+    rate: 2
+  },
+  {
+    title: "Ruby on Rails",
+    rate: 2
+  },
+  {
+    title: "Typescript",
+    rate: 4
+  },
+  {
+    title: "Gulp Js",
+    rate: 5
+  },
+  {
+    title: "Webpack",
+    rate: 3
+  },
+  {
+    title: "Bootstrap",
+    rate: 5
+  },
+  {
+    title: "SASS/LESS",
+    rate: 5
+  },
+  {
+    title: "Node Js",
+    rate: 3
+  },
+  {
+    title: "CSS/CSS3",
+    rate: 5
+  },
+  {
+    title: "HTML/HTML5",
+    rate: 5
+  },
+  {
+    title: "Git",
+    rate: 4
+  },
+  {
+    title: "Express Js",
+    rate: 3
+  },
+  {
+    title: "Adobe Photoshop",
+    rate: 3
+  },
+  {
+    title: "Adobe Illustrator",
+    rate: 2
+  },
+  {
+    title: "Adobe XD",
+    rate: 3
+  }
 ];
 
 const languages = [
-    {
-        title: "Persian",
-        rate: 5,
-    },
-    {
-        title: "English",
-        rate: 3,
-    },
+  {
+    title: "Persian",
+    rate: 5
+  },
+  {
+    title: "English",
+    rate: 3
+  }
 ];
 
 const Skill: React.FC = () => {
-    return (
-        <div className="skill">
-            <div className="skill__item">
-                <h2>Skills:</h2>
-                {skills.map((item, index) => (
-                    <SkillBox key={index} title={item.title} rate={item.rate} />
-                ))}
-            </div>
-            <div className="skill__item">
-                <h2>Languages:</h2>
-                {languages.map((item, index) => (
-                    <SkillBox key={index} title={item.title} rate={item.rate} />
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className="skill">
+      <div className="skill__item">
+        <h2>Skills:</h2>
+        {skills.map((item, index) => (
+          <SkillBox key={index} title={item.title} rate={item.rate} />
+        ))}
+      </div>
+      <div className="skill__item">
+        <h2>Languages:</h2>
+        {languages.map((item, index) => (
+          <SkillBox key={index} title={item.title} rate={item.rate} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Skill;
