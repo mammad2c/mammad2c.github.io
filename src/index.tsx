b9d7038f6aa1d10f2c623d377434dc4e32d1c46b
Mohammad Toosi <m_dd@rogers.com>
2022-12-01 20:34:26 +0330

    ref: update pwa

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { far } from "@fortawesome/free-regular-svg-icons";
@@ -9,8 +8,3 @@ library.add(fas, far);
 const container = document.getElementById("root")!;
 const root = createRoot(container);
 root.render(<App />);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorkerRegistration.register();
--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 import svgrPlugin from "vite-plugin-svgr";
+import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
-  resolve: {
-    alias: [
-      {
-        find: /^~.+/,
-        replacement: (val) => {
-          return val.replace(/^~/, "");
-        },
-      },
-    ],
+  build: {
+    outDir: path.resolve(__dirname, "build"),
   },
+  plugins: [
+    react(),
+    viteTsconfigPaths(),
+    svgrPlugin(),
+    VitePWA({
+      injectRegister: "auto",
+    }),
+  ],
 });
